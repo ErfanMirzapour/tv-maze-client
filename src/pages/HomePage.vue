40126ddb03e58a0caab1d45d780d62a7e7d5fbc0
Erfan Mirzapour <e.mirzapour@alibaba.ir>
2022-10-28 21:16:53 +0330

    feat: add pagination to homapage data

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,20 +1,45 @@
 <script setup lang="ts">
+import { ref, watch } from 'vue';
+import { RouterLink, useRoute } from 'vue-router';
 import Endpoints from '@/endpoints';
-import { RouterLink } from 'vue-router';
-import { fetch } from '../utils';
+import { Pagination } from '@/components';
+import { fetch } from '@/utils';
 
-const response = await fetch(Endpoints.SHOWS);
-const shows = await response.json();
+const fetchShows = (page = '1') =>
+   fetch(`${Endpoints.SHOWS}?page=${page}`).then(async res => {
+      console.time('Deserializing data');
+      const json = await res.json();
+      console.timeEnd('Deserializing data');
+
+      return json;
+   });
+
+const loading = ref(false);
+const shows = ref(await fetchShows());
+
+const route = useRoute();
+
+watch(
+   () => route.query.page,
+   async page => {
+      loading.value = true;
+      shows.value = await fetchShows(page as string);
+      loading.value = false;
+   },
+);
 </script>
 
 <template>
    <main>
-      <ul>
+      <span v-if="loading">Loading inside...</span>
+      <ul v-else>
          <li v-for="show in shows" :key="show.id">
             <RouterLink :to="{ name: 'show', params: { showId: show.id } }">
                {{ show.name }}
             </RouterLink>
          </li>
       </ul>
+
+      <Pagination :pages-count="10" />
    </main>
 </template>
